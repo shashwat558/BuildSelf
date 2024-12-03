f4f0fb16b65df87cb48cfe29b1e63aa860b577d8
shashwat558 <jainshashwat528@gmail.com>
2024-12-03 22:46:18 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+
+import './App.css'
+
+function App() {
+  <div>SelfBuild</div>
+  
+}
+
+export default App
---
+++ src/components/Graph.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Graph = () => {
+
+    const day = new Date()
+    console.log(day)
+  return (
+    <div>Graph</div>
+  )
+}
+
+export default Graph
